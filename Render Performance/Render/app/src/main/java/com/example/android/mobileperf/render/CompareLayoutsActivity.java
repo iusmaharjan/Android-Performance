f5e0d7b8287c9c9ba92bd229e6a444fac19a1d8d
Ayush Maharjan <ius.maharjan@gmail.com>
2016-05-06 14:49:37 +0545

    ComparingLayoutsActivity created

---
+++ Render Performance/Render/app/src/main/java/com/example/android/mobileperf/render/CompareLayoutsActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.mobileperf.render;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CompareLayoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_layouts);
+    }
+}
